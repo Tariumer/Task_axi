8dcf8037edffcfb7bac9c478ba8c1682b1c9b840
Tarium <toryanikov@axiomatika.ru>
2022-04-06 12:33:27 +0300

    Работа с бд,страницы принятия и отклонения кредита рандомным образом

--- Task/src/main/java/com/example/task/Peoples.java
+++ Task/src/main/java/com/example/task/Peoples.java
@@ -12,13 +12,94 @@ public class Peoples {
     private String name;
     private String surname;
     private String mid_name;
+    private String passport;
+    private String family_status;
+    private String address;
+    private String number;
+    private String organization;
+    private String post;
+    private Integer work_time;
+    private Integer credit_sum;
     public Peoples() {
     }
 
-    public Peoples(String name, String surname, String mid_name) {
+    public Peoples(String name, String surname, String mid_name, String passport, String family_status, String address, String number, String organization, String post, Integer work_time, Integer credit_sum) {
         this.name = name;
         this.surname = surname;
         this.mid_name = mid_name;
+        this.passport = passport;
+        this.family_status = family_status;
+        this.address = address;
+        this.number = number;
+        this.organization = organization;
+        this.post = post;
+        this.work_time = work_time;
+        this.credit_sum = credit_sum;
+    }
+
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getFamily_status() {
+        return family_status;
+    }
+
+    public void setFamily_status(String family_status) {
+        this.family_status = family_status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public Integer getWork_time() {
+        return work_time;
+    }
+
+    public void setWork_time(Integer work_time) {
+        this.work_time = work_time;
+    }
+
+    public Integer getCredit_sum() {
+        return credit_sum;
+    }
+
+    public void setCredit_sum(Integer credit_sum) {
+        this.credit_sum = credit_sum;
     }
 
     public String getName() {
--- Task/src/main/java/com/example/task/controller/HelloWorldController.java
+++ Task/src/main/java/com/example/task/controller/HelloWorldController.java
@@ -2,40 +2,69 @@ package com.example.task.controller;
 
 import com.example.task.Peoples;
 import com.example.task.service.PeopleService;
-import com.example.task.repos.PeopleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Controller
 public class HelloWorldController {
 
     PeopleService peopleService=new PeopleService();
-    @RequestMapping(value = "/greeting")
-    public String helloWorldController(Model model) {
-        model.addAttribute("name", "Dimon");
-        return "greeting";
-    }
+
     @GetMapping(value = "/index")
     public String index(Model model){
-        List<Peoples> peopleinfos = peopleService.findAllPeoples();
-        // добавить пользователей в modelmap или что то такое в thymeleaf
-        model.addAttribute("peopleinfos" ,peopleinfos);
         return "index";
     }
+
+    @GetMapping(value = "/displayAll")
+    public String display(Model model){
+        List<Peoples> peopleinfos = peopleService.findAllPeoples();
+        model.addAttribute("peopleinfos" ,peopleinfos);
+        return "displayAll";
+    }
+
     @GetMapping(value = "/add")
     public String hello(Model model) {
-        return "/add";
+        return "add";
     }
+
+    @GetMapping(value = "/accepting")
+    public String rnd(Model model){
+        return "/accepting";
+    }
+    @GetMapping(value = "/declaining")
+    public String tt(Model model){
+        return "/declaining";
+    }
+
+
     @PostMapping(value = "/add")
-    public String add(@RequestParam String name,@RequestParam String surname,@RequestParam String mid_name,Map<String,Object> model){
-        Peoples people= new Peoples(name,surname,mid_name);
+    public RedirectView add(@RequestParam String name,
+                      @RequestParam String surname,
+                      @RequestParam String mid_name,
+                      @RequestParam String passport,
+                      @RequestParam String family_status,
+                      @RequestParam String address,
+                      @RequestParam String number,
+                      @RequestParam String organization,
+                      @RequestParam String post,
+                      @RequestParam Integer work_time,
+                      @RequestParam Integer credit_sum,
+                      Map<String,Object> model){
+        Peoples people= new Peoples(name,surname,mid_name,passport,family_status,address,number,organization,post,work_time,credit_sum);
         peopleService.savePeople(people);
         // сделать редтрект
-        return "/index.html";
+        Random random=new Random();
+        if(random.nextInt(2)==0) {
+            return new RedirectView("declaining");
+        }
+        else
+        return new RedirectView("accepting");
     }
 }
